ed7aff9408633ea501a6934f208c5b92ec2a4739
somrat.dutta <somrat.dutta@nutanix.com>
2024-06-03 20:19:18 +0530

    Refactor NATS JetStream Source to use PayloadDeserializer - Replaced usage of DeserializationSchema with PayloadDeserializer in NatsJetStreamSource, NatsJetStreamSourceReader, and NatsJetStreamSourceBuilder. - Updated NatsJetstreamSourceTest to align with the new deserializer, replacing SimpleStringSchema with StringPayloadDeserializer. - Ensured compatibility with the existing NatsConsumerConfig for consumer configuration.
    These changes enhance the integration with NATS by utilizing the richer PayloadDeserializer interface, ensuring better handling of NATS message structures and metadata.

--- src/main/java/io/synadia/flink/source/NatsJetStreamSource.java
+++ src/main/java/io/synadia/flink/source/NatsJetStreamSource.java
@@ -5,11 +5,12 @@ package io.synadia.flink.source;
 
 import io.synadia.flink.Utils;
 import io.synadia.flink.common.ConnectionFactory;
+import io.synadia.flink.source.NatsConsumeOptions;
 import io.synadia.flink.source.enumerator.NatsSourceEnumerator;
 import io.synadia.flink.source.split.NatsSubjectCheckpointSerializer;
 import io.synadia.flink.source.split.NatsSubjectSplit;
 import io.synadia.flink.source.split.NatsSubjectSplitSerializer;
-import org.apache.flink.api.common.serialization.DeserializationSchema;
+import io.synadia.flink.payload.PayloadDeserializer;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.connector.source.*;
 import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
@@ -25,16 +26,16 @@ public class NatsJetStreamSource<OutputT> implements Source<OutputT, NatsSubject
 
     private final ConnectionFactory connectionFactory;
     private final String natsSubject;
-    private final DeserializationSchema<OutputT> deserializationSchema;
+    private final PayloadDeserializer<OutputT> payloadDeserializer;
     private final NatsConsumeOptions config;
     private static final Logger LOG = LoggerFactory.getLogger(NatsJetStreamSource.class);
     private final String id;
     private final Boundedness mode;
 
     // Package-private constructor to ensure usage of the Builder for object creation
-    NatsJetStreamSource(DeserializationSchema<OutputT> deserializationSchema, ConnectionFactory connectionFactory, String natsSubject, NatsConsumeOptions config, Boundedness mode) {
+    NatsJetStreamSource(PayloadDeserializer<OutputT> payloadDeserializer, ConnectionFactory connectionFactory, String natsSubject, NatsConsumeOptions config, Boundedness mode) {
         id = Utils.generateId();
-        this.deserializationSchema = deserializationSchema;
+        this.payloadDeserializer = payloadDeserializer;
         this.connectionFactory = connectionFactory;
         this.natsSubject = natsSubject;
         this.config = config;
@@ -43,7 +44,7 @@ public class NatsJetStreamSource<OutputT> implements Source<OutputT, NatsSubject
 
     @Override
     public TypeInformation<OutputT> getProducedType() {
-        return this.deserializationSchema.getProducedType();
+        return payloadDeserializer.getProducedType();
     }
 
     @Override
@@ -83,7 +84,7 @@ public class NatsJetStreamSource<OutputT> implements Source<OutputT, NatsSubject
     @Override
     public SourceReader<OutputT, NatsSubjectSplit> createReader(SourceReaderContext readerContext) throws Exception {
         LOG.debug("{} | createReader", id);
-        return new NatsJetStreamSourceReader<>(id, connectionFactory, config, deserializationSchema, readerContext, natsSubject, mode);
+        return new NatsJetStreamSourceReader<>(id, connectionFactory, config, payloadDeserializer, readerContext, natsSubject, mode);
     }
 
     @Override
@@ -91,7 +92,7 @@ public class NatsJetStreamSource<OutputT> implements Source<OutputT, NatsSubject
         return "NatsSource{" +
                 "id='" + id + '\'' +
                 ", subjects=" + natsSubject +
-                ", payloadDeserializer=" + deserializationSchema.getClass().getCanonicalName() +
+                ", payloadDeserializer=" + payloadDeserializer.getClass().getCanonicalName() +
                 ", connectionFactory=" + connectionFactory +
                 '}';
     }
